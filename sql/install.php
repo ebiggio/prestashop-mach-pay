e00477d333cd80a8572602e631caf0958a18809f
Enzo Biggio <ebiggio@gmail.com>
2023-01-19 03:30:12 -0300

    Corrección de script SQL ejecutado durante la instalación

--- sql/install.php
+++ sql/install.php
@@ -3,9 +3,9 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'machpay` (
             `id_machpay` INT(11) NOT NULL AUTO_INCREMENT,
             `id_cart` INT(11) NOT NULL,
             `cart_total` INT(11) NOT NULL,
-            `business_payment_id` VARCHAR(60) DEFAULT NOT NULL,
-            `machpay_webhook_event` VARCHAR(50) DEFAULT NULL NULL,
-            `machpay_created_at` datetime DEFAULT NOT NULL,
+            `business_payment_id` VARCHAR(60) NOT NULL,
+            `machpay_webhook_event` VARCHAR(50) NULL NULL,
+            `machpay_created_at` datetime NOT NULL,
             `date_add` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY (`id_machpay`)
         ) ENGINE=' . _MYSQL_ENGINE_ . '
@@ -14,8 +14,8 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'machpay` (
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'machpay_webhook_event` (
             `id_machpay_webhook_event` INT(11) NOT NULL AUTO_INCREMENT,
             `event_upstream_id` VARCHAR(30) NOT NULL,
-            `business_payment_id` VARCHAR(60) DEFAULT NOT NULL,
-            `event_name` VARCHAR(50) DEFAULT NULL NULL,
+            `business_payment_id` VARCHAR(60) NOT NULL,
+            `event_name` VARCHAR(50) NULL NULL,
             `date_add` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY (`id_machpay_webhook_event`)
         ) ENGINE=' . _MYSQL_ENGINE_ . '
