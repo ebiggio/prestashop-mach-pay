337e14fb91364498b726c2f4e81c544aa53ac87d
Enzo Biggio <ebiggio@gmail.com>
2023-01-19 02:07:37 -0300

    Cambio en nombres de columnas de tabla que almacena los eventos de webhook, para ser equivalentes con las descripciones que usa MACH Pay

--- sql/install.php
+++ sql/install.php
@@ -13,8 +13,9 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'machpay` (
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'machpay_webhook_event` (
             `id_machpay_webhook_event` INT(11) NOT NULL AUTO_INCREMENT,
-            `id_cart` INT(11) NOT NULL,
-            `machpay_webhook_event` VARCHAR(50) DEFAULT NULL NULL,
+            `event_upstream_id` VARCHAR(30) NOT NULL,
+            `business_payment_id` VARCHAR(60) DEFAULT NOT NULL,
+            `event_name` VARCHAR(50) DEFAULT NULL NULL,
             `date_add` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY (`id_machpay_webhook_event`)
         ) ENGINE=' . _MYSQL_ENGINE_ . '
